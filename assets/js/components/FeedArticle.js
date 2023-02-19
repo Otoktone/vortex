6eb43cccb9ea9fb8523918c13aef2af979b4961b
Alex Otoktone <alexandre.desmot@gmail.com>
2023-02-19 12:10:49 +0100

    font: update feed articles and modal component

--- assets/js/components/FeedArticle.js
+++ assets/js/components/FeedArticle.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
 import React from 'react';
 
+// api url (develop)(TODO : change with env varibale)
 const apiUrl = 'http://localhost:80/api/feed/articles';
 
 class FeedArticles extends React.Component {
+  // init state
   state = {
     articles: []
   };
 
+  // wait until component is mounted
   async componentDidMount() {
     try {
+      // axios client request
       const res = await axios.get(apiUrl);
+      // update state with articles
       this.setState({ articles: res.data });
     } catch (error) {
       console.error(error);
@@ -18,16 +23,18 @@ class FeedArticles extends React.Component {
   }
 
   render() {
+    // populate state with articles from axios call
     const { articles } = this.state;
 
     return (
       <div className="feedContainer">
+        {/* create each article from articles */}
         {articles.map(article => (
           <div className="feedItem" key={article.id}>
             <a href={article.link} target="_blank">
               <div className="feedTitle">
-                <p>{article.category}</p>
-                <h3>{article.title}</h3>
+                <p>{article.category[0].toUpperCase() + article.category.substr(1).toLowerCase()}<i className="fa-solid fa-bookmark"></i></p>
+                <h3>{article.title[0].toUpperCase() + article.title.substr(1).toLowerCase()}</h3>
               </div>
               <div className="feedImage">
                 <img src={article.media || 'build/images/icons/favicon.png'} alt={article.title} />

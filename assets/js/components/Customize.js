56f21ea6de1e0c6f75feaafd82ee92bc6642466a
Alex Otoktone <alexandre.desmot@gmail.com>
2023-04-02 08:15:40 +0200

    feat: display categories and save user choices

--- assets/js/components/Customize.js
+++ assets/js/components/Customize.js
@@ -1,22 +1,115 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+const apiUrl = "http://localhost:80/api";
 
 function MyModal() {
   const [showModal, setShowModal] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [user, setUserId] = useState(null);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`${apiUrl}/category/list`);
+        const data = await response.json();
+        console.log('API response:', data);
+        if (Array.isArray(data.categories)) {
+          setCategories(data.categories);
+          setUserId(data.user);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchCategories();
+  }, []);
 
   const handleModal = () => {
-    // onclick check state modal
     setShowModal(!showModal);
+  };
+
+  const handleCheckboxChange = (event) => {
+    const categoryId = parseInt(event.target.value);
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      setSelectedCategories([...selectedCategories, categoryId]);
+    } else {
+      setSelectedCategories(
+        selectedCategories.filter((id) => id !== categoryId)
+      );
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    try {
+      const data = {
+        categories: selectedCategories.join(","),
+        user: user,
+      };
+      const urlParams = new URLSearchParams({
+          categories: selectedCategories.join(","),
+          user: user,
+      });
+      const response = await fetch(`${apiUrl}/user/category/${urlParams.get('categories')}/${urlParams.get('user')}`, {
+          method: "GET",
+          headers: {
+              "Content-Type": "application/json",
+          },
+      });
+      setLoading(false);
+      setSuccess(true);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
   };
 
   return (
     <>
-        <a href="#" onClick={handleModal}>
-                <i className="fa-solid fa-gear"></i><span>Customize</span>
-        </a>
-        {showModal && (
+      <a href="#" onClick={handleModal}>
+        <i className="fa-solid fa-gear"></i>
+        <span>Customize</span>
+      </a>
+      {showModal && (
         <div className="modal-overlay">
           <div className="modal-content">
-            <p>Custom your feed : </p>
+            <form onSubmit={handleSubmit}>
+              <p>Custom your feed : </p>
+              {loading ? (
+                <p>Loading categories...</p>
+              ) : (
+                <ul>
+                  {categories ? (
+                    categories.map((category) => (
+                      <li key={category.id}>
+                        <input
+                          type="checkbox"
+                          id={`category-${category.id}`}
+                          value={category.id}
+                          checked={selectedCategories.includes(category.id)}
+                          onChange={handleCheckboxChange}
+                        />
+                        <label htmlFor={`category-${category.id}`}>
+                          {category.name}
+                        </label>
+                      </li>
+                    ))
+                  ) : null}
+                </ul>
+              )}
+              {success && (
+                <p>Categories saved successfully</p>
+              )}
+              <button type="submit">Save</button>
+            </form>
             <button onClick={handleModal}>Close</button>
           </div>
         </div>
---
+++ src/Controller/Front/User/UserCategoryController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller\Front\User;
+
+use App\Entity\Category;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Security;
+
+class UserCategoryController extends AbstractController
+{
+    private EntityManagerInterface $entityManager;
+    private Security $security;
+
+    public function __construct(EntityManagerInterface $entityManager, Security $security)
+    {
+        $this->entityManager = $entityManager;
+        $this->security = $security;
+    }
+
+    #[Route('/api/user/category/{categories}/{user}', name: 'api_user_category', methods: ['GET'])]
+    public function saveUserCategories(string $categories, string $user)
+    {
+        // fetch data send by form
+        $categoryIds = array_filter(explode(',', $categories));
+        if ($categoryIds === null) {
+            throw new \InvalidArgumentException('Categories parameter is missing');
+        }
+
+        // get user data from db
+        $user = $this->entityManager->getRepository(User::class)->find($user);
+
+        // clear category list current user
+        $user->getCategories()->clear();
+
+        // update categories selected by current user
+        foreach ($categoryIds as $categoryId) {
+            $category = $this->entityManager->getRepository(Category::class)->find($categoryId);
+            if ($category !== null) {
+                $user->addCategory($category);
+            } else {
+                throw new \InvalidArgumentException(sprintf('Category with ID %d not found', $categoryId));
+            }
+        }
+        $this->entityManager->flush();
+
+        $categories = $user->getCategories()->map(function ($category) {
+            return ['id' => $category->getId(), 'name' => $category->getName()];
+        })->toArray();
+
+        return new JsonResponse(['success' => true, 'user' => $user->getId(), 'categories' => $categories, 'message' => 'Categories updated successfully']);
+    }
+}

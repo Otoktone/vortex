1a8d0a58eda37da92eedeef7822a63ea919eb185
Otoktone <alexandre.desmot@gmail.com>
2023-05-22 11:51:07 +0200

    front(dashboard): add modal on article

--- assets/js/components/FeedArticle.js
+++ assets/js/components/FeedArticle.js
@@ -6,7 +6,11 @@ const apiUrl = 'http://localhost:80/api/feed/articles';
 class FeedArticles extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { articles: [] };
+    this.state = {
+      articles: [],
+      selectedArticle: null,
+      showModal: false
+    };
   }
   // init state
   // state = {
@@ -27,34 +31,64 @@ class FeedArticles extends React.Component {
     }
   }
 
+  openModal = (article) => {
+    this.setState({ selectedArticle: article, showModal: true });
+  }
+
+  closeModal = () => {
+    this.setState({ selectedArticle: null, showModal: false });
+  }
+
   render() {
     // populate state with articles from fetch call
-    const { articles } = this.state;
+    const { articles, selectedArticle, showModal } = this.state;
 
     return (
       <div className="feedContainer">
-        {/* create each article from articles */}
         {articles.map(article => (
-          <div className="feedItem" key={article.id}>
+          <div className="feedItem" key={article.id} onClick={() => this.openModal(article)}>
             <div>
-              <a href={article.link} target="_blank">
-                <div className="feedTitle">
-                  <p>{article.category[0].toUpperCase() + article.category.substr(1).toLowerCase()}<i className="fa-solid fa-bookmark"></i></p>
-                  <h3>{article.title[0].toUpperCase() + article.title.substr(1).toLowerCase().slice(0, 100)}...</h3>
-                </div>
-                <div className="feedImage">
-                  <img src={article.media || 'build/images/icons/favicon.png'} alt={article.title} />
-                </div>
-                <div className="feedContent">
-                  <p>{article.content.slice(0, 100)}...</p>
-                </div>
-                <div className="feedDateContainer">
-                  <div className="feedDate">{article.date}</div>
-                </div>
-              </a>
+              <div className="feedTitle">
+                <p>{article.category[0].toUpperCase() + article.category.substr(1).toLowerCase()}</p>
+                <h3>{article.title[0].toUpperCase() + article.title.substr(1).toLowerCase().slice(0, 100)}...</h3>
+              </div>
+              <div className="feedImage">
+                <img src={article.media || 'build/images/icons/favicon.png'} alt={article.title} />
+              </div>
+              <div className="feedContent">
+                <p>{article.content.slice(0, 100)}...</p>
+              </div>
+              <div className="feedDateContainer">
+                <div className="feedDate">{article.date}</div>
+              </div>
             </div>
           </div>
         ))}
+
+        {selectedArticle && showModal && (
+          <div className="modal">
+            <div className="modalContent">
+              <div className="modalTitle">
+                <p>{selectedArticle.category[0].toUpperCase() + selectedArticle.category.substr(1).toLowerCase()}</p>
+                <h3>{selectedArticle.title[0].toUpperCase() + selectedArticle.title.substr(1).toLowerCase()}</h3>
+              </div>
+              <div className="modalImage">
+                <img src={selectedArticle.media || 'build/images/icons/favicon.png'} alt={selectedArticle.title} />
+              </div>
+              <div className="">
+                <p>{selectedArticle.content}</p>
+              </div>
+              <div className="modalDateContainer">
+                <div className="modalDate">{selectedArticle.date}</div>
+              </div>
+              <div className="modalLink">
+                <a className="home_button" href={selectedArticle.link} target="_blank"><i className="fa-solid fa-external-link"></i> Open article</a>
+                <span className="home_button"><i className="fa-solid fa-bookmark"></i> Add to bookmars</span>
+              </div>
+              <span className="close" onClick={this.closeModal}>&times;</span>
+            </div>
+          </div>
+        )}
       </div>
     );
   }

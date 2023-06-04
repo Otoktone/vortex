0d5b257fe29eb5c29e45ac755a3fed29f885d846
Otoktone <alexandre.desmot@gmail.com>
2023-06-04 09:21:09 +0200

    fix(back): check feed url format

--- src/Controller/Back/Admin/FeedCrudController.php
+++ src/Controller/Back/Admin/FeedCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Back\Admin;
 
 use App\Entity\Feed;
 use App\Entity\FeedArticle;
+use Symfony\Component\Validator\Constraints\Url;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Form\Type\FileUploadType;
@@ -12,7 +13,7 @@ use Symfony\Component\Form\{FormBuilderInterface, FormEvents};
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use Symfony\Component\Form\Extension\Core\Type\{PasswordType, RepeatedType};
 use EasyCorp\Bundle\EasyAdminBundle\Config\{Crud, KeyValueStore, Action, Actions};
-use EasyCorp\Bundle\EasyAdminBundle\Field\{ChoiceField, IdField, EmailField, ImageField, TextField};
+use EasyCorp\Bundle\EasyAdminBundle\Field\{ChoiceField, IdField, EmailField, ImageField, TextField, UrlField};
 
 class FeedCrudController extends AbstractCrudController
 {
@@ -33,7 +34,16 @@ class FeedCrudController extends AbstractCrudController
         $fields = [
             IdField::new('id')->hideOnForm(),
             TextField::new('name')->setLabel('Flux'),
-            TextField::new('url')->setLabel('Url'),
+
+            // Check url validity
+            TextField::new('url')
+                ->setLabel('Url')
+                ->setFormTypeOptions([
+                    'constraints' => [
+                        new Url(),
+                    ],
+                ]),
+
             // ImageField::new('imageFile')->setLabel('Image')->setBasePath('build/images/users/')->setUploadDir('assets/images/users/')->setFormType(FileUploadType::class)->setUploadedFileNamePattern('[randomhash].[extension]'),
             // ChoiceField::new('roles')->allowMultipleChoices(true)->setChoices([
             //     'Utilisateur' => 'ROLE_USER',

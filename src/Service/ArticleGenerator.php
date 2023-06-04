a8163a32f820dc6c9fcacaa62ec15ccdc511dc56
Otoktone <alexandre.desmot@gmail.com>
2023-06-04 15:41:14 +0200

    back(service): update article generator

--- src/Service/ArticleGenerator.php
+++ src/Service/ArticleGenerator.php
@@ -34,104 +34,112 @@ class ArticleGenerator
         } catch (\Exception $e) {
             // echo 'Caught exception : ',  $e->getMessage(), "\n";
             // return false;
-            throw new \Exception('Caught exception: ' . $e->getMessage());
+            throw new \Exception('Caught exception with method isValidUrl : ' . $e->getMessage());
         }
         return true;
     }
 
     public function generateArticleFromRss()
     {
-        $feeds = $this->entityManager->getRepository(Feed::class)->findAll();
+        try {
+            $feeds = $this->entityManager->getRepository(Feed::class)->findAll();
 
 
-        // check url validity before generating articles
-        foreach ($feeds as $feed) {
-            if ($this->isValidFeedUrl($feed->getUrl())) {
-                $this->validFeeds[] = $feed;
+            // check url validity before generating articles
+            foreach ($feeds as $feed) {
+                if ($this->isValidFeedUrl($feed->getUrl())) {
+                    $this->validFeeds[] = $feed;
+                }
             }
-        }
 
-        // get current year in order to filter articles
-        $currentYear = (int) (new \DateTime())->format('Y');
+            // get current year in order to filter articles
+            $currentYear = (int) (new \DateTime())->format('Y');
 
-        $items = [];
-        $filteredItems = [];
-        foreach ($this->validFeeds as $feed) {
-            if ($this->isValidFeedUrl($feed->getUrl())) {
-                // if url is valid then read the feed
-                $result = $this->feedIo->read($feed->getUrl());
-                $currentFeed = $result->getFeed();
-                foreach ($currentFeed as $item) {
-                    $categories = $item->getCategories();
-                    foreach ($categories as $category) {
-                        $categoryTerm = $category->getTerm();
+            $items = [];
+            $filteredItems = [];
+            foreach ($this->validFeeds as $feed) {
+                if ($this->isValidFeedUrl($feed->getUrl())) {
+                    // if url is valid then read the feed
+                    $result = $this->feedIo->read($feed->getUrl());
+                    $currentFeed = $result->getFeed();
+                    foreach ($currentFeed as $item) {
+                        $categories = $item->getCategories();
+                        foreach ($categories as $category) {
+                            $categoryTerm = $category->getTerm();
 
-                        $existingCategory = $this->entityManager->getRepository(Category::class)->findOneBy(['name' => $categoryTerm]);
+                            $existingCategory = $this->entityManager->getRepository(Category::class)->findOneBy(['name' => $categoryTerm]);
 
-                        // create categories if it doesnt exit yet
-                        if (!$existingCategory) {
-                            $newCategory = new Category();
-                            $newCategory->setName($categoryTerm);
-                            $this->entityManager->persist($newCategory);
-                            $this->entityManager->flush();
-                        }
-                    }
-                    $mediaImage = null;
-                    $content = $item->getContent();
-                    // check if there is media balise, otherwise look for img balise inside content
-                    preg_match('/<img.+src=[\'"](?P<src>.+?)[\'"].*>/i', $content, $image);
-                    if (isset($image['src'])) {
-                        $mediaImage = ['imageUrl' => $image['src']];
-                    } else {
-                        foreach ($item->getMedias() as $media) {
-                            $imageUrl = $media->getUrl();
-                            if (!empty($imageUrl)) {
-                                $mediaImage = ['imageUrl' => $imageUrl];
+                            // create categories if it doesnt exit yet
+                            if (!$existingCategory) {
+                                $newCategory = new Category();
+                                $newCategory->setName($categoryTerm);
+                                $this->entityManager->persist($newCategory);
+                                $this->entityManager->flush();
                             }
                         }
-                    }
-                    // remove html balises and manage special characters
-                    $content = html_entity_decode(strip_tags($item->getContent()), ENT_QUOTES | ENT_HTML5, 'UTF-8');
-                    $date = $item->getLastModified();
-                    $itemYear = (int) $date->format('Y');
-                    if ($itemYear === $currentYear) {
-                        $items[] = [
-                            'title' => $item->getTitle(),
-                            'content' => $content,
-                            'link' => $item->getLink(),
-                            'date' => $date,
-                            'media' => $mediaImage,
-                            'category' => $categoryTerm
-                        ];
-                    }
-                    $currentDate = new \DateTime();
-                    $lastMonth = $currentDate->modify('-1 month');
+                        $mediaImage = null;
+                        $content = $item->getContent();
+                        // check if there is media balise, otherwise look for img balise inside content
+                        preg_match('/<img.+src=[\'"](?P<src>.+?)[\'"].*>/i', $content, $image);
+                        if (isset($image['src'])) {
+                            $mediaImage = ['imageUrl' => $image['src']];
+                        } else {
+                            foreach ($item->getMedias() as $media) {
+                                $imageUrl = $media->getUrl();
+                                if (!empty($imageUrl)) {
+                                    $mediaImage = ['imageUrl' => $imageUrl];
+                                }
+                            }
+                        }
+                        // remove html balises and manage special characters
+                        $content = html_entity_decode(strip_tags($item->getContent()), ENT_QUOTES | ENT_HTML5, 'UTF-8');
+                        $date = $item->getLastModified();
+                        $itemYear = (int) $date->format('Y');
+                        if ($itemYear === $currentYear) {
+                            $items[] = [
+                                'title' => $item->getTitle(),
+                                'content' => $content,
+                                'link' => $item->getLink(),
+                                'date' => $date,
+                                'media' => $mediaImage,
+                                'category' => $categoryTerm
+                            ];
+                        }
+                        $currentDate = new \DateTime();
+                        $lastMonth = $currentDate->modify('-1 month');
 
-                    $filteredItems = array_filter($items, function ($item) use ($lastMonth) {
-                        return $item['date'] >= $lastMonth;
-                    });
+                        $filteredItems = array_filter($items, function ($item) use ($lastMonth) {
+                            return $item['date'] >= $lastMonth;
+                        });
+                    }
                 }
             }
-        }
-        foreach ($filteredItems as $filteredItem) {
-            // check if article is already registered into db
-            $existingArticle = $this->feedArticleRepository->findOneBy(['link' => $filteredItem['link']]);
-            if (!$existingArticle) {
-                $newFilteredItem = new FeedArticle();
-                $newFilteredItem->setTitle($filteredItem['title']);
-                $newFilteredItem->setContent($filteredItem['content']);
-                $newFilteredItem->setLink($filteredItem['link']);
-                $newFilteredItem->setDate($filteredItem['date']->format('Y-m-d H:i:s'));
-                if (isset($filteredItem['media']['imageUrl'])) {
-                    $newFilteredItem->setMedia($filteredItem['media']['imageUrl']);
-                } else {
-                    $newFilteredItem->setMedia(null);
+            foreach ($filteredItems as $filteredItem) {
+                // check if article is not empty
+                if (empty($filteredItem['title']) || empty($filteredItem['content']) || empty($filteredItem['link'])) {
+                    continue;
                 }
-                $newFilteredItem->setCategory($filteredItem['category']);
-                $this->entityManager->persist($newFilteredItem);
+                // check if article is already registered into db
+                $existingArticle = $this->feedArticleRepository->findOneBy(['link' => $filteredItem['link']]);
+                if (!$existingArticle) {
+                    $newFilteredItem = new FeedArticle();
+                    $newFilteredItem->setTitle($filteredItem['title']);
+                    $newFilteredItem->setContent($filteredItem['content']);
+                    $newFilteredItem->setLink($filteredItem['link']);
+                    $newFilteredItem->setDate($filteredItem['date']->format('Y-m-d H:i:s'));
+                    if (isset($filteredItem['media']['imageUrl'])) {
+                        $newFilteredItem->setMedia($filteredItem['media']['imageUrl']);
+                    } else {
+                        $newFilteredItem->setMedia(null);
+                    }
+                    $newFilteredItem->setCategory($filteredItem['category']);
+                    $this->entityManager->persist($newFilteredItem);
+                }
             }
+            $this->entityManager->flush();
+        } catch (\Exception $e) {
+            throw new \Exception('Caught exception with method generateArticleFromRss : ' . $e->getMessage());
         }
-        $this->entityManager->flush();
     }
 
     public function generateArticle()
